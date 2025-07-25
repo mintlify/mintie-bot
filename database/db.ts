65645c780c36fdad4343f46011eca1f9ffeeaff4
Vidyoot Senthil <vidyoots@gmail.com>
2025-07-25 16:36:43 -0700

    fix: use cryptr

--- database/db.ts
+++ database/db.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-import crypto from "crypto";
+import Cryptr from "cryptr";
 import { EventType } from "../types";
 import { logEvent } from "../utils/logging";
 import { usersSchema } from "./SlackUser";
@@ -14,26 +14,14 @@ if (!encryptionKey) {
   throw new Error("ENCRYPTION_LOCAL_KEY is not set in environment variables");
 }
 
+const cryptr = new Cryptr(encryptionKey);
+
 const encrypt = (text: string): string => {
-  const algorithm = "aes-256-cbc";
-  const key = crypto.scryptSync(encryptionKey, "salt", 32);
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return iv.toString("hex") + ":" + encrypted;
+  return cryptr.encrypt(text);
 };
 
 const decrypt = (encryptedText: string): string => {
-  const algorithm = "aes-256-cbc";
-  const key = crypto.scryptSync(encryptionKey, "salt", 32);
-  const textParts = encryptedText.split(":");
-  const iv = Buffer.from(textParts.shift() || "", "hex");
-  const encryptedData = textParts.join(":");
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decrypted = decipher.update(encryptedData, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
+  return cryptr.decrypt(encryptedText);
 };
 
 const connect = async () => {
